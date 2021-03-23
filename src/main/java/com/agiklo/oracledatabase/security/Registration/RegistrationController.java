f0c62b6fcbc5ae26e465465a13e7f705d0126e18
agiklo <54153910+agiklo@users.noreply.github.com>
2021-03-23 21:41:02 +0100

    feat: Add rank restrictions that can create and confirm an account

--- src/main/java/com/agiklo/oracledatabase/security/Registration/RegistrationController.java
+++ src/main/java/com/agiklo/oracledatabase/security/Registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.agiklo.oracledatabase.security.Registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +16,14 @@ public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
 
+
     @GetMapping(path = "confirm")
+    @PreAuthorize("hasAuthority('EMPLOYEE') or hasAuthority('MANAGER') or hasAuthority('ADMIN')")
     public String confirm(@RequestParam("token") String token) {
         return registrationService.confirmToken(token);
     }
