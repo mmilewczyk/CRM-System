fc8532f69da1148839aef14c123cbf05ec7b5461
agiklo <54153910+agiklo@users.noreply.github.com>
2021-05-03 18:13:16 +0200

    test: Create new tests for EmailService

---
+++ src/test/java/com/agiklo/oracledatabase/security/email/EmailServiceTest.java
@@ -0,0 +1,54 @@
+package com.agiklo.oracledatabase.security.email;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ContextConfiguration(classes = {EmailService.class})
+@ExtendWith(SpringExtension.class)
+class EmailServiceTest {
+    @Autowired
+    private EmailService emailService;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void shouldSendEmail() throws MailException {
+        // when
+        doNothing().when(this.javaMailSender).send((MimeMessage) any());
+        when(this.javaMailSender.createMimeMessage()).thenReturn(new MimeMessage((Session) null));
+        this.emailService.send("izabela@agiklocrm.com", "mateusz@agiklocrm.com");
+
+        // then
+        verify(this.javaMailSender).createMimeMessage();
+        verify(this.javaMailSender).send((MimeMessage) any());
+    }
+
+    @Test
+    void shouldNotSendEmailAndThrowsException() throws MailException {
+        // when
+        doNothing().when(this.javaMailSender).send((MimeMessage) any());
+        when(this.javaMailSender.createMimeMessage()).thenReturn(new MimeMessage((Session) null));
+
+        // then
+        assertThrows(IllegalStateException.class,
+                () -> this.emailService.send("Confirm your email", "mateusz@agiklocrm.com"));
+        verify(this.javaMailSender).createMimeMessage();
+    }
+}
+
